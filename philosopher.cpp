151abdd5656242b97abff629356a77fa6066e8b9
Geoffrey Cline <gdcwd5@mail.mst.edu>
2015-10-25 14:50:08 -0500

    breakdown philosopher

---
+++ philosopher.cpp
@@ -0,0 +1,51 @@
+#include <cstdlib>
+#include <iostream>
+#include <cerrno>
+#include <unistd.h>
+#include <list>
+
+using namespace std;
+
+#include "mpi.h"
+
+const int MAX_TOTAL_EAT = 10;
+const int MAX_EAT_SESSION = 10;
+
+void philosopher()
+{
+  int eat_quota = rand() % MAX_TOTAL_EAT;
+  int msgIn, msgOut;
+  int tag = 1;
+  const int id = MPI::COMM_WORLD.Get_rank ( );
+  MPI::Status status;
+
+  while(eat_quota > 0)
+  {
+    cout << "wating : " << id << endl; 
+    sleep(rand() % 4);
+    cout << "done wating : " << id << endl; 
+
+    //request forks
+    msgOut = eat_quota;
+    std::cout << "Request: " << id << " | M: " << msgOut << std::endl;
+    MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, 0, tag ); 
+    
+    //wait for forks
+    MPI::COMM_WORLD.Recv ( &msgIn, 1, MPI::INT, 0, tag, status );
+    std::cout << "Fufilled: " << id << " | M: " << msgOut << std::endl;
+
+    //eat 
+    cout << "eating : " << id << endl; 
+    toeat = rand() % 10;
+    sleep(toeat);
+    eat_quota -= toeat;
+    cout << "done eating : " << id << endl;
+
+    //return forks
+    msgOut = -1;
+    std::cout << "Reporting Return: " << id << " | M: " << msgOut << std::endl;
+    MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, 0, tag ); 
+  }
+
+  return;
+}
--- program.cpp
+++ program.cpp
@@ -14,16 +14,14 @@ void philosopher();
 
 int main ( int argc, char *argv[] ) 
 {
-  int id, eat_quota;
-  int tag = 1;
+  int id;
   MPI::Status status;
-  int msgOut, msgIn;
-  int toeat;
 
   MPI::Init ( argc, argv );
   
   //Safety check - need at least 2 philosophers to make sense
-  if (MPI::COMM_WORLD.Get_size ( ) < 3) {
+  if (MPI::COMM_WORLD.Get_size ( ) < 3) 
+  {
 	    MPI::Finalize ( );
 	    std::cerr << "Need at least 2 philosophers! Try again" << std::endl;
 	    return 1; //non-normal exit
@@ -35,50 +33,9 @@ int main ( int argc, char *argv[] )
   
   //  Setup Fork Master (Ombudsman) and Philosophers
   if ( id == 0 ) //Master
-  {
     ombudsman();
-    
-	}
   else //I'm a philosopher
-  {
-
-    eat_quota = rand() % 30;
-
-    while(eat_quota > 0)
-    {
-      cout << "wating : " << id << endl; 
-      sleep(rand() % 10);
-      cout << "done wating : " << id << endl; 
-
-      //request forks
-      msgOut = eat_quota;
-      std::cout << "Request: " << id << " | M: " << msgOut << std::endl;
-      MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, 0, tag ); 
-      
-      //wait for forks
-      MPI::COMM_WORLD.Recv ( &msgIn, 1, MPI::INT, 0, tag, status );
-      std::cout << "Fufilled: " << id << " | M: " << msgOut << std::endl;
-
-      //eat 
-      cout << "eating : " << id << endl; 
-      toeat = rand() % 10;
-      sleep(toeat);
-      eat_quota -= toeat;
-      cout << "done eating : " << id << endl;
-
-      //return forks
-      msgOut = -1;
-      std::cout << "Return: " << id << " | M: " << msgOut << std::endl;
-      MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, 0, tag ); 
-    }
-	
-    
-    //return forks
-    msgOut = -1;
-    std::cout << "Full: " << id << " | M: " << msgOut << std::endl;
-    MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, 0, tag ); 
-
-  }
+    philosopher();
   
   //  Terminate MPI.
   MPI::Finalize ( );
